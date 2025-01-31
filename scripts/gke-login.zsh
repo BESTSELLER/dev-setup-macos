01ac644087def19472ceee3c066be699be2fc9cc
Lasse Gaardsholt <lasse.gaardsholt@bestseller.com>
2025-01-31 12:05:48 +0100

    cleaned up the gke-login script
    Signed-off-by: Lasse Gaardsholt <lasse.gaardsholt@bestseller.com>

--- scripts/gke-login.zsh
+++ scripts/gke-login.zsh
@@ -2,39 +2,33 @@
 function gke-login {
   currentContext=$(kubectl config current-context)
   allProjects=$(gcloud projects list --format=json | jq -c '.[] | { name: .name, id: .projectId }')
-  
-  for row in $(echo "${allProjects}" | jq -r -c '. | @base64'); do
-    decoded=$(echo ${row} | base64 --decode)
-    projectId=$(echo ${decoded} | jq '.id'|  tr -d \")
-    projectName=$(echo ${decoded} | jq '.name' | tr -d \")
-    clusterArray=$(gcloud container clusters list --project=$projectId --format=json | jq -c '.[] | { name: .name, zone: .zone }')
 
+  gcloud projects list --format=json | jq -c '.[] | { name: .name, id: .projectId }' |  while read -r accountList; do
+    projectId=$(echo ${accountList} | jq '.id'|  tr -d \")
+    projectName=$(echo ${accountList} | jq '.name'|  tr -d \")
+    clusterArray=$()
 
-    if [ ${#clusterArray} -gt 0 ]
-    then
-      echo && printf "\e[1m\e[32m$projectName \e[0m\n"
-      for cluster in $(echo "${clusterArray}" | jq -r -c '. | @base64'); do
-        decoded=$(echo ${cluster} | base64 --decode)
-        clusterName=$(echo ${decoded} | jq '.name' | tr -d \")
-        clusterZone=$(echo ${decoded} | jq '.zone' | tr -d \")
+    echo && printf "\e[1m\e[32m$projectName \e[0m\n"
+    gcloud container clusters list --project=$projectId --format=json | jq -c '.[] | { name: .name, zone: .zone }' | while read -r clusters; do
+      clusterName=$(echo ${clusters} | jq '.name' | tr -d \")
+      clusterZone=$(echo ${clusters} | jq '.zone' | tr -d \")
 
-        echo $clusterName
+      echo $clusterName
 
-        if [[ -n $(kubectl config view -o jsonpath='{.contexts[?(@.name == "'$clusterName'")]}') ]]
-        then
-          echo "cluster is there, skipping"
-          continue
-        fi
+      if [[ -n $(kubectl config view -o jsonpath='{.contexts[?(@.name == "'$clusterName'")]}') ]]
+      then
+        echo "cluster is there, skipping"
+        continue
+      fi
 
-        gcloud container clusters get-credentials $clusterName --zone $clusterZone --project=$projectId
-        
-        currentCluster=$(kubectx -c)
-        a=("${(@s/_/)currentCluster}")
-        newName="$a[-1]"
-        kubectx $newName=$currentCluster > /dev/null
+      gcloud container clusters get-credentials $clusterName --zone $clusterZone --project=$projectId
 
-      done
-    fi
+      currentCluster=$(kubectx -c)
+      a=("${(@s/_/)currentCluster}")
+      newName="$a[-1]"
+      kubectx $newName=$currentCluster > /dev/null
+
+    done
 
   done
   kubectl config use-context $currentContext > /dev/null
